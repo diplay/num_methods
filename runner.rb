a8a0d439e71787b3fff1d1f113215c7fa8f5d227
diplay <diplaySuper@gmail.com>
2014-10-27 15:47:42 +0300

    runner improved - time

--- runner.rb
+++ runner.rb
@@ -5,6 +5,19 @@ require 'json'
 
 mat_cnt = 100
 mat_n = 3
+eps = 1e-5
+
+ARGV.each do |arg|
+  case arg
+  when /^n=(\d+)$/
+    mat_n = $1.to_i
+  when /^eps=([\d\.e\-]+)$/
+    eps = $1.to_f
+  when /^cnt=(\d+)$/
+    mat_cnt = $1.to_i
+  end
+end
+
 puts 'Генерация матриц'
 `echo -ne "" > matrices.txt`
 `echo -ne "" > jacobi.txt`
@@ -14,12 +27,33 @@ puts 'Генерация матриц'
 end
 
 puts 'Запуск тестов метода Якоби'
+start_time = Time.now
+sum = 0
 (1..mat_cnt).each do |i|
-  iter = `cat matrices.txt | awk 'NR >= #{3*i-2} && NR <= #{3*i}' | ./laba1.rb output=json output=only_iterations`
+  iter = `cat matrices.txt | awk 'NR >= #{mat_n*(i-1) + 1} && NR <= #{mat_n*i}' | ./laba1.rb eps=#{eps} output=only_iterations`
+  sum += iter.to_i
   `echo "#{iter}" >> jacobi.txt`
 end
+finish_time = Time.now
+time = finish_time - start_time
+sum /= mat_cnt
+time /= mat_cnt
+time *= 1000
+puts "Среднее количество итераций: #{sum}"
+puts "Среднее время работы: #{time} ms"
+
 puts 'Запуск тестов метода QR'
+start_time = Time.now
+sum = 0
 (1..mat_cnt).each do |i|
-  iter = `cat matrices.txt | awk 'NR >= #{3*i-2} && NR <= #{3*i}' | ./laba1.rb method=qr output=json output=only_iterations`
+  iter = `cat matrices.txt | awk 'NR >= #{mat_n*(i-1) + 1} && NR <= #{mat_n*i}' | ./laba1.rb method=qr eps=#{eps} output=only_iterations`
+  sum += iter.to_i
   `echo "#{iter}" >> qr.txt`
 end
+finish_time = Time.now
+time = finish_time - start_time
+sum /= mat_cnt
+time /= mat_cnt
+time *= 1000
+puts "Среднее количество итераций: #{sum}"
+puts "Среднее время работы: #{time} ms"
