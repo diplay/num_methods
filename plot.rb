d1579c2a87c948c6afa0a00a82de037c5857b744
diplay <diplaySuper@gmail.com>
2014-10-29 16:12:13 +0300

    statistics plots

---
+++ plot.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/ruby
+#coding: utf-8
+
+require 'gnuplot'
+
+def plot_file(filename, method)
+  data = []
+  File.foreach(filename) do |line|
+    data << line.split(' ').each_with_index.collect {|el, index| index == 1 ? el.to_f : el.to_i}
+  end
+
+  n = data.collect{ |el| el[0] }
+  iter = data.collect{ |el| el[2] }
+  time = data.collect{ |el| el[1] }
+  err = data.collect { |el| Math::sqrt(el[3]) }
+
+  Gnuplot.open do |gp|
+    Gnuplot::Plot.new(gp) do |plot|
+      plot.terminal 'pdf'
+      plot.output "#{method}.pdf"
+      plot.title "Метод #{method}"
+      plot.encoding 'utf8'
+      plot.xrange "[1:#{data.last[0] + 1}]"
+      plot.xlabel "Размерность"
+      plot.ylabel "Итерации"
+      plot.data << Gnuplot::DataSet.new([n, iter, err]) do |ds|
+        ds.with = 'errorb'
+        ds.notitle
+      end
+      plot.data << Gnuplot::DataSet.new([n, iter]) do |ds|
+        ds.with = 'lines'
+        ds.linewidth = 2
+        ds.title = "Среднее число итераций"
+      end
+    end
+
+    Gnuplot::Plot.new(gp) do |plot|
+      plot.title "Среднее время работы, метод #{method}"
+      plot.xrange "[1:#{data.last[0] + 1}]"
+      plot.xlabel "Размерность"
+      plot.ylabel "Время работы, мс"
+      plot.data << Gnuplot::DataSet.new([n, time]) do |ds|
+        ds.with = 'lines'
+        ds.linewidth = 2
+        ds.title = "Среднее время работы"
+      end
+    end
+  end
+
+end
+
+filename = nil
+method = 'undefined'
+
+ARGV.each do |arg|
+  case arg
+  when /^method=(.+)$/
+    method = $1
+  when /^file=(.+)$/
+    filename = $1
+  end
+end
+
+plot_file(filename, method)
--- runner.rb
+++ runner.rb
@@ -5,7 +5,6 @@ mat_cnt = 100
 mat_n = 3
 eps = 1e-5
 mat_n_from = nil
-delimiter = ' '
 
 ARGV.each do |arg|
   case arg
@@ -17,8 +16,6 @@ ARGV.each do |arg|
     mat_cnt = $1.to_i
   when /^from-n=(\d+)$/
     mat_n_from = $1.to_i
-  when /^delimiter=(.)$/
-    delimiter = $1
   end
 end
 
@@ -34,38 +31,34 @@ puts 'Генерация матриц'
   end
 end
 
-(mat_n_from..mat_n).each do |n|
-  puts "Запуск тестов метода Якоби для n=#{n}"
+def run_tests(method, n, mat_cnt, eps, mat_n)
   start_time = Time.now
-  sum = 0
+  sum = []
   (1..mat_cnt).each do |i|
-    iter = `cat matrices#{n}.csv | awk 'NR >= #{n*(i-1) + 1} && NR <= #{n*i}' | ./laba1.rb eps=#{eps} output=only-iterations`
-    sum += iter.to_i
-    `echo "#{n}#{delimiter}#{eps}#{delimiter}#{iter}" >> jacobi.csv`
+    iter = `cat matrices#{n}.csv | awk 'NR >= #{n*(i-1) + 1} && NR <= #{n*i}' | ./laba1.rb eps=#{eps} method=#{method} output=only-iterations`
+    sum << iter.to_i
   end
   finish_time = Time.now
   time = finish_time - start_time
-  sum /= mat_cnt
   time /= mat_cnt
   time *= 1000
-  puts "Среднее количество итераций: #{sum}"
+  avr = sum.reduce(:+) / mat_cnt
+  disp = sum.reduce {|disp, el| (el - avr)*(el - avr)} / (mat_n - 1)
+  puts "Среднее количество итераций: #{avr}"
+  puts "Дисперсия количества итераций: #{disp}"
   puts "Среднее время работы: #{time} ms"
+  `echo "#{n} #{time} #{avr} #{disp}" >> #{method}.csv`
+end
+
+(mat_n_from..mat_n).each do |n|
+  puts "Запуск тестов метода Якоби для n=#{n}"
+  run_tests('jacobi', n, mat_cnt, eps, mat_n)
 end
 
 (mat_n_from..mat_n).each do |n|
   puts "Запуск тестов метода QR для n=#{n}"
-  start_time = Time.now
-  sum = 0
-  (1..mat_cnt).each do |i|
-    iter = `cat matrices#{n}.csv | awk 'NR >= #{n*(i-1) + 1} && NR <= #{n*i}' | ./laba1.rb method=qr eps=#{eps} output=only-iterations`
-    sum += iter.to_i
-    `echo "#{n}#{delimiter}#{eps}#{delimiter}#{iter}" >> qr.csv`
-  end
-  finish_time = Time.now
-  time = finish_time - start_time
-  sum /= mat_cnt
-  time /= mat_cnt
-  time *= 1000
-  puts "Среднее количество итераций: #{sum}"
-  puts "Среднее время работы: #{time} ms"
+  run_tests('qr', n, mat_cnt, eps, mat_n)
 end
+
+`./plot.rb file=qr.csv method=QR`
+`./plot.rb file=jacobi.csv method=Якоби`
